60a09e089facefa7630956d678b9dada473b790b
RobielFvaj <robiel.tesfazghi.eclat@fvaj.ch>
2021-03-08 10:36:01 +0100

    add register/login/logout form

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>L'ordi Utile</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body class="bg-gray-200">
+  <nav class="p-6 bg-white flex justify-between mb-6 md:p-8">
+    <ul class="flex items-center">
+      <li>
+        <a href="/" class="p-3">Home</a>
+      </li>
+      @auth        
+      <li>
+        <a href="{{ route('compte') }}" class="p-3">Mes Compte</a>
+      </li>  
+      @endauth
+    </ul>
+    
+    <ul class="flex items-center"> 
+      @auth
+        <li>
+          <a href="{{ route('profile') }}" class="p-3">Profile</a>
+        </li>    
+        <li>
+          <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+              @csrf
+              <button type="submit">Logout</button>
+          </form>
+        </li>
+        <li>
+          <a href="{{ route('register') }}" class="p-3">Register</a>
+        </li>
+      @endauth 
+      @guest
+        <li>
+          <a href="{{ route('login') }}" class="p-3">Login</a>
+        </li>    
+      @endguest
+    </ul>
+  </nav>
+
+
+
+  @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CompteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function () { 
+  return view('home');
+})->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register', [RegisterController::class, 'index'])
+->name('register')
+->middleware('auth');
+Route::post('register',[RegisterController::class, 'store']);
+
+Route::get('/compte', [CompteController::class, 'index'])
+->name('compte')
+->middleware('auth');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login',[LoginController::class, 'store']);
+
+use App\Http\Controllers\Auth\LogoutController;
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
+
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('/profile',[ProfileController::class, 'store']);
