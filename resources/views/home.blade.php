7ed9e89072b1f02d587084012bb4996a4b09b04a
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-04-28 14:17:54 +0200

    :sparkles: set working environment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,9 @@
         <div class="w-8/12 bg-white p-6 rounded-lg">
             Bienvenue
         </div>
+
+       <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </div>
     </div>
 @endsection
